8a9c4aff11fa3d153eee253797aa654e275e9137
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-29 00:35:41 -0700

    created method to get the missing number in the array

--- src/MissingNumberInArray.java
+++ src/MissingNumberInArray.java
@@ -1,5 +1,14 @@
+//array contain no.'s form 1-n-1 as one of the number is missing and there is no repetetion.
+
 public class MissingNumberInArray {
-    
+  public static int missingNumberInArray(int arr[], int n){
+      int sum1 = n*(n+1)/2;
+      int sum2 = 0;
+      for(int i = 0; i<n; i++){
+          sum2 += arr[i];
+      }
+      return sum1 - sum2;
+  }
 
     public static void main(String[] args) {
 
