340d21491b5aa2bbfc54624f013b24aa16b61d7f
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-29 00:38:07 -0700

    added comments for better understanding

--- src/MissingNumberInArray.java
+++ src/MissingNumberInArray.java
@@ -2,11 +2,15 @@
 
 public class MissingNumberInArray {
   public static int missingNumberInArray(int arr[], int n){
+
+      //lets get the sum of first n numbers
       int sum1 = n*(n+1)/2;
       int sum2 = 0;
+      //run a loop to get the sum of the numbers sum of the numbers present in the array
       for(int i = 0; i<n; i++){
           sum2 += arr[i];
       }
+      //subtract the sum of the no.'s in array from the sum of n numbers.
       return sum1 - sum2;
   }
 
